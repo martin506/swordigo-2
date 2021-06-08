583ec7aa2d8d1ed9a6365e74279904de199d2992
Martynas Simanavicius <martynas.simanavicius2@gmail.com>
2021-06-08 16:18:19 +0300

    animations for running

--- Swordigas/Assets/Scripts/Player.cs
+++ Swordigas/Assets/Scripts/Player.cs
@@ -6,25 +6,46 @@ public class Player : MonoBehaviour
 {
     public float speed;
     private Vector3 scaleChange = new Vector3(8f, 0f, 0f);
+    public Rigidbody2D rigidBody2D;
+
+    private Animator animator;
 
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        Running();
+    }
+
+    private void Running()
+    {
         if (Input.GetAxis("Horizontal") > 0)
         {
-            Vector2 newTransform = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
-            transform.position = newTransform;
+            Vector2 newTransform = new Vector2(speed, rigidBody2D.velocity.y);
+            rigidBody2D.velocity = newTransform;
             transform.localScale = new Vector3(4f, 4f, 1f);
         }
-        if (Input.GetAxis("Horizontal") < 0)
+        else if (Input.GetAxis("Horizontal") < 0)
         {
-            Vector2 newTransform = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
-            transform.position = newTransform;
+            Vector2 newTransform = new Vector2(-speed, rigidBody2D.velocity.y);
+            rigidBody2D.velocity = newTransform;
             transform.localScale = new Vector3(-4f, 4f, 1f);
+        }
+        else
+        {
+            rigidBody2D.velocity = new Vector2(0f, rigidBody2D.velocity.y);
+        }
+
+        if (rigidBody2D.velocity.x != 0)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
         }
     }
 }
